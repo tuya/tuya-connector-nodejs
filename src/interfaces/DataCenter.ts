954ddf805bd6c7f42c3429f5a97253b6874dcdae
Daniel Sales <dsa21may@gmail.com>
2022-04-20 18:37:02 +0200

    feat(enum): add TuyaDataCenter enum

---
+++ src/interfaces/DataCenter.ts
@@ -0,0 +1,10 @@
+enum TuyaDataCenter {
+  CHINA = 'https://openapi.tuyacn.com',
+  WESTERN_AMERICA = 'https://openapi.tuyaus.com',
+  EASTERN_AMERICA = 'https://openapi-ueaz.tuyaus.com',
+  CENTRAL_EUROPE = 'https://openapi.tuyaeu.com',
+  WESTERN_EUROPE = 'https://openapi-weaz.tuyaeu.com',
+  INDIA = 'https://openapi.tuyain.com',
+}
+
+export default TuyaDataCenter;
